197d1035e2a5d8d4f0a015326ab818279c0b6af4
José Ismael <100933555+ismaelGTI@users.noreply.github.com>
2025-04-02 16:26:53 +0200

    Update example3.sh

--- Microservicios/example3.sh
+++ Microservicios/example3.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-echo "Este es un script de ejemploms3."
+
+# Imprimir un mensaje indicando que el script se está ejecutando
+echo "Ejecutando example3.sh..."
+
+# Listar los archivos en el directorio actual
+echo "Listando archivos en el directorio actual:"
+ls -la
+
+# Fin del script
+echo "example3.sh ha terminado."
